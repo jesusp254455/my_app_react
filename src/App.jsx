4fab2eab7eb1e4c2364ca14b3a8856c883024295
jesusp254455 <jpenaranda633gmail.com>
2023-02-03 15:55:50 -0500

    jp

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+
+
 
 
 
 function App() {
 
-  let num = (n)=>{
-   document.getElementById('pant2').value = document.getElementById('pant2').value + n;
+  const [num, setnum] = useState('');
+
   
-    }
 
-  return (
+
+ const Num =(e)=>{
+    setnum(num + e.target.value);
     
-    <>  
-     <div class="content">
+ }
 
-
-     <input type="text" id="pant1" disabled />
-      <br />
-      <input type="text" id="pant2" disabled />
-    <hr/>
-<table>
-  <tr>
-    <th> <button onClick={() => num(7)}>7</button> </th>
-    <th> <button  onClick={() => num(8)}>8</button> </th>
-    <th> <button  onClick={() => num(9)}>9</button> </th>
-    <th> <button  onClick={() => num("/")}>/</button> </th>
-  </tr>
-  <tr>
-    <th> <button  onClick={() => num(4)}>4</button> </th>
-    <th> <button  onClick={() => num(5)}>5</button> </th>
-    <th> <button  onClick={() => num(6)}>6</button> </th>
-    <th> <button  onClick={() => num("x")} >x</button> </th>
-  </tr>
-  <tr>
-    <th> <button  onClick={() => num(1)} >1</button> </th>
-    <th> <button  onClick={() => num(2)}>2</button> </th>
-    <th> <button  onClick={() => num(3)}>3</button> </th>
-    <th> <button  onClick={() => num("-")}>-</button> </th>
-  </tr>
-  <tr>
-    <th> <button  onClick={() => num(0)} >0</button> </th>
-    <th> <button  onClick={() => num(",")} >,</button> </th>
-    <th> <button   class="igual">=</button> </th>
-    <th> <button  onClick={() => num("+")} >+</button> </th>
-  </tr>
-  </table>
-    </div>
-  </>
+ const Signo1 = (e)=>{
+  if (condition) {
+    
+  } else {
+    
+  }
+ }
  
-  )
+
+function Igual(props) {
+
+  return <>
+          <button class="igual" >{props.value}</button>
+  </>
+
 }
 
-export default App
+function Boton(props) {
+
+ 
+  return <> 
+  
+  <button onClick={Num} value={props.valor}>{props.valor}</button>
+  
+  </>
+}
+
+function Signo(props) {
+
+ 
+  return <> 
+  
+  <button onClick={Signo1} value={props.sg}>{props.sg}</button>
+  
+  </>
+}
+
+
+
+
+
+
+
+ 
+  return (
+    <>
+      <div class="content">
+      <input type="text" id="pant1"    disabled />
+      <input type="text" id="pant1"  value={num}  disabled />
+        <hr/>
+        <table>
+          <tr>
+          <th> <Boton  valor="7"/> </th>
+          <th> <Boton valor="8"/> </th>
+          <th> <Boton valor="9"/> </th>
+          <th> <Signo sg="/"/> </th>
+          </tr>
+          <tr>
+          <th> <Boton valor="4"/> </th>
+          <th> <Boton valor="5"/> </th>
+          <th> <Boton valor="6"/> </th>
+          <th> <Signo sg="x"/> </th>
+          </tr>
+          <tr>
+          <th> <Boton valor="1"/> </th>
+          <th> <Boton valor="2"/> </th>
+          <th> <Boton valor="3"/> </th>
+          <th> <Signo sg="-"/> </th>
+          </tr>
+          <tr>
+          <th> <Boton valor="0"/> </th>
+          <th> <Boton valor=","/> </th>
+          <th> <Igual  value="="/> </th>
+          <th> <Signo  sg="+"/> </th>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
+}
+
+
+export default App;
