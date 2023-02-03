93f6daad9777e0c4f763622dd0f97e83de034651
jesusp25 <jpenarnada633@gmail.com>
2023-02-03 12:02:03 -0500

    jp

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,9 @@ import './App.css'
 
 
 function App() {
-  var num = (n)=>{
-    
-    const [cityName, setCityName] = useState(n);
-  
 
+  let num = (n)=>{
+   document.getElementById('pant2').value = document.getElementById('pant2').value + n;
   
     }
 
@@ -17,8 +15,7 @@ function App() {
     
     <>  
      <div class="content">
-    
-     <h2>{cityName}</h2>
+
 
      <input type="text" id="pant1" disabled />
       <br />
@@ -27,27 +24,27 @@ function App() {
 <table>
   <tr>
     <th> <button onClick={() => num(7)}>7</button> </th>
-    <th> <button>8</button> </th>
-    <th> <button>9</button> </th>
-    <th> <button >/</button> </th>
+    <th> <button  onClick={() => num(8)}>8</button> </th>
+    <th> <button  onClick={() => num(9)}>9</button> </th>
+    <th> <button  onClick={() => num("/")}>/</button> </th>
   </tr>
   <tr>
-    <th> <button>4</button> </th>
-    <th> <button>5</button> </th>
-    <th> <button>6</button> </th>
-    <th> <button >x</button> </th>
+    <th> <button  onClick={() => num(4)}>4</button> </th>
+    <th> <button  onClick={() => num(5)}>5</button> </th>
+    <th> <button  onClick={() => num(6)}>6</button> </th>
+    <th> <button  onClick={() => num("x")} >x</button> </th>
   </tr>
   <tr>
-    <th> <button >1</button> </th>
-    <th> <button>2</button> </th>
-    <th> <button>3</button> </th>
-    <th> <button>-</button> </th>
+    <th> <button  onClick={() => num(1)} >1</button> </th>
+    <th> <button  onClick={() => num(2)}>2</button> </th>
+    <th> <button  onClick={() => num(3)}>3</button> </th>
+    <th> <button  onClick={() => num("-")}>-</button> </th>
   </tr>
   <tr>
-    <th> <button >0</button> </th>
-    <th> <button>,</button> </th>
-    <th> <button  class="igual">=</button> </th>
-    <th> <button >+</button> </th>
+    <th> <button  onClick={() => num(0)} >0</button> </th>
+    <th> <button  onClick={() => num(",")} >,</button> </th>
+    <th> <button   class="igual">=</button> </th>
+    <th> <button  onClick={() => num("+")} >+</button> </th>
   </tr>
   </table>
     </div>
